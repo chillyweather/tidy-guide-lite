5049ba4351a6a89326f0f39dd0fc953171869847
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-12-08 12:07:37 +0200

    update

--- src/figma_functions/Anatomy/anatomyData/addEffectsInfo.ts
+++ src/figma_functions/Anatomy/anatomyData/addEffectsInfo.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getEffects, EffectResult } from "./getEffects";
 import { setTextContent } from "../../utilityFunctions";
+import { buildAutoLayoutFrame } from "../../utilityFunctions";
 
 export async function addEffectsInfo(
   frame: any,
@@ -10,13 +11,26 @@ export async function addEffectsInfo(
   const effects: EffectResult | null = await getEffects(frame);
   if (!effects) return;
 
-  const tag = tagComponent.findOne((node) => node.name === "type=info");
-  if (!(tag && tag.type === "COMPONENT")) return;
-
-  const effectsDataElement = createEffectsDataElement(tag);
-
   const effectNames = Object.keys(effects);
   console.log("effectNames", effectNames);
+  if (effectNames.includes("style")) {
+    console.log("yep!!!");
+  }
+
+  const tag = tagComponent.findOne((node) => node.name === "type=info");
+  if (!(tag && tag.type === "COMPONENT")) return;
+  const effectsDataElement = createEffectsDataElement(tag);
+
+  const indexElement = buildAutoLayoutFrame(
+    "indexElement",
+    "VERTICAL",
+    0,
+    0,
+    8
+  );
+  indexElement.appendChild(effectsDataElement);
+  indexes.appendChild(indexElement);
+
   effectNames.forEach((effectName) => {
     const indexInfo = figma.createFrame();
     indexInfo.name = `.${effectName}`;
